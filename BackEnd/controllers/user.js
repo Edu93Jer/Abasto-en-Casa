ed9ac87d3e86c22de17c2b62fa9ff8661db0f932
edu93 <mailing_93@hotmail.com>
2020-08-24 16:09:14 -0500

    Updating Dependecies

--- BackEnd/controllers/user.js
+++ BackEnd/controllers/user.js
@@ -1,24 +1,25 @@
-const User = require( '../models/User')
+const User = require('../models/User')
 const Mailbox = require('../models/Mailbox')
-exports.profileGet = async ( req, res ) => {
+
+exports.profileGet = async (req, res) => {
  const { _id } = req.user
- const profile = await User.findById( _id ).populate( 'orders' )
- res.status( 200 ).json({ profile })
+ const profile = await User.findById(_id).populate('orders')
+ res.status(200).json({ profile })
 }
 
-exports.profileEdit = async ( req, res ) => {
+exports.profileEdit = async (req, res) => {
  const { _id } = req.user
- const profile = await User.findByIdAndUpdate( _id , { $set: { ...req.body }}, { new: true })
- res.status( 200 ).json({ profile })
+ const profile = await User.findByIdAndUpdate(_id, { $set: { ...req.body } }, { new: true })
+ res.status(200).json({ profile })
 }
 
-exports.profileDel = async ( req, res ) => {
+exports.profileDel = async (req, res) => {
  const { _id } = req.user
- const profile = await User.findByIdAndDelete( _id )
- res.status( 200 ).json({ profile })
+ const profile = await User.findByIdAndDelete(_id)
+ res.status(200).json({ profile })
 }
 
-exports.createMessage = async ( req, res ) => {
+exports.createMessage = async (req, res) => {
  const message = await Mailbox.create({ ...req.body })
  res.status(201).json({ message })
 }
